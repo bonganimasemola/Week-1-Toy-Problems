d84f37f523c02067d0f17de05daf0e83a9850912
bonganimasemola <bonganimasemola@icloud.com>
2023-09-25 15:40:50 +0300

    completed Week 1 Toy Problems

--- indexchallenge1.js
+++ indexchallenge1.js
@@ -4,6 +4,8 @@
 function studentMarks (score) {
     if (score > 80) {
         return "A";
+
+//      problem set hints at multiple options in terms of choice for the grader, thus trying if...else statements in a function
     } else if (score >= 60 && score <= 70) {
         return "B";
     } else if (score >= 50 && score <= 59) {
--- indexchallenge2.js
+++ indexchallenge2.js
@@ -1,10 +1,11 @@
 /* Solution: Challenge 2: Speed Detector (Toy Problem) */
 
 function speedDetector(speed) {
-
+// clearly assigning using Let for the events, the events are speed limit (either the driver is driving below, at or above the limit && demeritPoints for when the driver is driving bove the limit of 70)
     let speedLimit = 70;
     let demeritPoints = 0;
 
+//  problem set presents option for detecting speed thus trying if... else statements within a function 
     if (speed <= speedLimit) {
         return ("Ok"); 
     } else { 
